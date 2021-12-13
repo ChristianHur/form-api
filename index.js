15d24893cd97a479de5450cd3c417347d1052ce9
Christian Hur <chrisvhur@gmail.com>
2021-12-12 19:49:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { urlencoded } = require('express');
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`http://localhost:${port}`))
 app.use(cors())
+const { users } = require('./models/mock_data')
 
 //parsing JSON
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +18,26 @@ app.get('/', (req, res) => {
     res.sendFile('index.html')
 })
 
+app.get('/api/', (req, res) => {
+    res.status(200);
+    res.json({ users });
+})
+
+app.get('/api/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let index = users.findIndex(record => record.id == id)
+    if (index != -1){
+        res.status(200);
+        res.json({ "user":users[index] });
+    }
+    res.status(404);
+    res.json("No record found.");
+})
+
 app.post('/api/', (req, res) => {
     const data = req.body;
     if (Object.keys(data).length != 0) {
+        users.push(data);
         res.status(200);
         res.json({ "status": "Record Added", data });
     } else {
@@ -28,22 +46,28 @@ app.post('/api/', (req, res) => {
     }
 })
 
-app.put('/api/:id', (req, res) => {
+app.put('/api/:id', (req, res) => {    
     const data = req.body;
-    if (Object.keys(data).length != 0) {
-        res.status(200);
+    const id = parseInt(req.params.id);
+    let index = users.findIndex(record => record.id == id)
+    if (index != -1){
+        users[index]=data;
+        res.status(200);        
         res.json({ "status": "Record Updated", data });
     } else {
         res.status(404)
-        res.json("None Data Received.");
+        res.json("No data found.  Can't update.");
     }
 })
 
 app.delete('/api/:id', (req, res) => {
-    const id = req.params.id;
-    if (id.length != 0) {
+    const id = parseInt(req.params.id);
+    let index = users.findIndex(record => record.id == id)
+    if (index != -1){
+        let temp = users[index];
+        users.splice(index,1);
         res.status(200);
-        res.json({ "status": "One Record Deleted", data });
+        res.json({ "status": "One Record Deleted", temp });
     } else {
         res.status(404)
         res.json("No record found.");
@@ -51,6 +75,7 @@ app.delete('/api/:id', (req, res) => {
 })
 
 app.delete('/api/', (req, res) => {
+    users.splice(0);
     res.status(200);
-    res.json({ "status": "All Records Deleted", data });
+    res.json({ "status": "All Records Deleted", users });
 })
