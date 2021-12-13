549b0d2fed90355df178246c39661664b6524945
Christian Hur <chrisvhur@gmail.com>
2021-12-12 18:45:07 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const { urlencoded } = require('express');
+const port = process.env.port || 9000;
+app.listen(port, () => console.log(`http://localhost:${port}`))
+app.use(cors())
+
+//parsing JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('YAH')
+})
+
+app.post('/api/', (req, res) => {
+    const data = req.body;
+    if (Object.keys(data).length != 0) {
+        res.status(200);
+        res.json({ "status": "Record Added", data });
+    } else {
+        res.status(404)
+        res.json("None Data Received.");
+    }
+})
+
+app.put('/api/:id', (req, res) => {
+    const data = req.body;
+    if (Object.keys(data).length != 0) {
+        res.status(200);
+        res.json({ "status": "Record Updated", data });
+    } else {
+        res.status(404)
+        res.json("None Data Received.");
+    }
+})
+
+app.delete('/api/:id', (req, res) => {
+    const id = req.params.id;
+    if (id.length != 0) {
+        res.status(200);
+        res.json({ "status": "One Record Deleted", data });
+    } else {
+        res.status(404)
+        res.json("No record found.");
+    }
+})
+
+app.delete('/api/', (req, res) => {
+    res.status(200);
+    res.json({ "status": "All Records Deleted", data });
+})
